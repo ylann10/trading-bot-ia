dc9a44ff422dc9d8a3fa0fbc75f5ab0259d85200
Ylann Hebert <yhebert@ics.fr>
2024-04-11 12:09:51 +0200

    Stopping beyond a certain result

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+from environment import *
+from keras.callbacks import EarlyStopping
 from os.path import exists
 from stats import getStats
 from pprint import pprint
-from environment import *
 from time import sleep
 from os import system
 from sys import exit
@@ -12,6 +13,8 @@ import numpy as np
 
 MODEL = 'model.keras'
 DATA_LIMIT = 3650
+MAX_EPOCHS = None
+MAX_LOSS = 0.01
 
 model = Sequential()
 model.add(layers.Dense(units=1, input_shape=(6,)))
@@ -23,19 +26,19 @@ model.add(layers.Dense(units=1))
 if exists(MODEL):
     model = models.load_model(MODEL)
 else:
+    counter = 0
     stats = getStats(DATA_LIMIT)
     entree = np.array(stats['x'], dtype=float)
     sortie = np.array(stats['y'], dtype=float)
     model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(x=entree, y=sortie, epochs=10000)
+    while True:
+        history = model.fit(x=entree, y=sortie, epochs=1, verbose=0)
+        counter += 1
+        print(f"Epochs: {counter}; Loss: {history.history['loss'][0]}")
+        if history.history['loss'][0] <= MAX_LOSS or (MAX_EPOCHS and counter >= MAX_EPOCHS):
+            break
     model.save(MODEL)
 
 x = np.array(getStats(1, True), dtype=float)
 r = model.predict(x, verbose=None)[0][0]
-print('Open: ' + str(x[0][0]))
-print('High: ' + str(x[0][1]))
-print('Low: ' + str(x[0][2]))
-print('Close: ' + str(x[0][3]))
-print('Volume: ' + str(x[0][4]))
-print('Trades: ' + str(x[0][5]))
 print('Next close (predicted): ' + str(r))
